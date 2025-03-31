85a586828d895647f43fc420c19bb2e9dec9e2a3
blackabdoul <156062722+blackabdoul@users.noreply.github.com>
2025-03-31 18:49:09 +0000

    second comit

--- src/02/Challenge/ch2_sort.cpp
+++ src/02/Challenge/ch2_sort.cpp
@@ -16,9 +16,23 @@
 // Returns: A boolean value: True on success, false otherwise.
 bool sort_array(int *arr, int n){
 
-    // Write your code here
+    if(arr==NULL)
+        return false;
+    if(n<2)
+        return true;
 
-    return false;
+    bool sorted = false;
+    int temp;
+    while (!sorted){
+        sorted = true;
+        if(arr[i] > arr[i+1])
+            temp == arr[i];
+            arr[i] == arr[i+1];
+            arr[i+1]==temp;
+            sorted == false;
+    }
+
+    return true;
 }
 
 // Main function
